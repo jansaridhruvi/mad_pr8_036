1a901a7fb13dc9e9e82d96999696ae5da2f5c7da
student <student@gnu.ac.in>
2023-09-26 10:46:37 +0530

    pr8

--- app/src/main/java/com/example/mad_pr8_036/MainActivity.kt
+++ app/src/main/java/com/example/mad_pr8_036/MainActivity.kt
@@ -4,23 +4,33 @@ import android.app.PendingIntent
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.widget.TextClock
+import android.widget.Toast
+import com.example.mad_pr8_036.databinding.ActivityMainBinding
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-    }
-    fun set_alarm(militime:Long,action:String)
-    {
-        val intent_alarm=Intent(applicationContext,AlarmBroadcastReciever::class.java)
-        intent_alarm.putExtra(AlarmBroadcastReciever.ALARMKEY)
-        val pendingintent=PendingIntent.getBroadcast(applicationContext,3103,intent_alarm,PendingIntent)
-        val manager=getSystemService(ALARM_SERVICE) as AlarmManager
-        if(action==AlarmBroadcastReciever.alarm_stop)
+        var textClock: TextClock = findViewById<TextClock>(R.id.Textclock)
+        var btn: FloatingActionButton = findViewById<FloatingActionButton>(R.id.btn1)
+        btn.setOnClickListener()
         {
-            manager.setExact(AlarmManager.RTC_WAKEUP,militime,pendingintent)
-        }
 
+        }
+        textClock.setOnClickListener()
+        {
+            val d = textClock.text
+            Toast.makeText(this, d, Toast.LENGTH_LONG).show()
+        }
+        fun set_alarm(militime: Long, action: String) {
+            val intent_alarm = Intent(applicationContext, AlarmBroadcastReciever::class.java)
+            intent_alarm.putExtra(AlarmBroadcastReciever.ALARMKEY, action)
+            val pendingintent = PendingIntent.getBroadcast(applicationContext, 3103, intent_alarm,pendingintent)
+            val manager = getSystemService(ALARM_SERVICE) as AlarmManager
+            if (action == AlarmBroadcastReciever.alarm_stop) {
+                manager.setExact(AlarmManager.RTC_WAKEUP, militime, pendingintent)
+            }
+        }
     }
 }
